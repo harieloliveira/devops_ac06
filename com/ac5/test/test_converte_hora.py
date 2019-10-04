0a361ee0429032b7635fca099c3331d9439d1b41
igsilvs <igor.ssantos@aluno.faculdadeimpacta.com.br>
2019-10-03 22:01:36 -0300

    é

---
+++ com/ac5/test/test_converte_hora.py
@@ -0,0 +1,21 @@
+import pytest
+from com.ac06.converte_hora import converteHora
+
+
+def test_hora():
+    assert converteHora(24, 60) is None, 'Should be None'
+
+
+def test_hora2():
+    assert converteHora(5, 10) == '05:10 AM'
+
+
+def test_hora3():
+    assert converteHora(13, 10) == '01:10 PM'
+
+
+def test_hora4():
+    assert converteHora(0, 0) == '12:00 AM'
+
+
+pytest.freeze_includes()
